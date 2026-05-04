d2e7a47440d77eccf9c07566e18d671ea2f22723
Reza Gohari <253446@usn.no>
2026-05-04 14:35:42 +0200

    Initial commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace TransitInsight.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
